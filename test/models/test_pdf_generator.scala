b50459cc550f944da9bf21b4c32400ce34788f3b
Franck Verrot <franck@verrot.fr>
2012-04-07 09:14:13 +0200

    Without assertion the block result isnt a value Result class.

--- test/models/test_pdf_generator.scala
+++ test/models/test_pdf_generator.scala
@@ -10,11 +10,12 @@ class TestPdfGenerator extends Specification {
   "takes two parameters" in {
     val f = File.createTempFile("foo", "bar")
     val pg = new PdfGenerator(f,f)
+    pg must not beNull
   }
 
   "has a generate method" in {
     val f = File.createTempFile("foo", "bar")
     val pg = new PdfGenerator(f,f)
-    pg.generate(f)
+    pg.generate(f) must not beNull
   }
 }
